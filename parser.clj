d3c57c2d44cf2b923943742b4d0764df51dfe160
Lok Chi Hon <lhon4644@bths.edu>
2019-07-18 19:59:52 -0400

    sendr/setr/liftr

--- parser.clj
+++ parser.clj
@@ -1,25 +1,31 @@
 (ns grammar.parser)
 
 ;Global configuation variable
-(def global-config (atom {:state ""
-                          :string ""
-                          :registers (atom {})
-                          :hold-register '()
-                          :level ""
-                          :pop-config ""
-                          :push-acts ""
-                          :sendr-acts (atom {})
-                          :liftr-acts (atom {} )}))
+(def ^{:dynamic true} *config* nil)
+;(def global-config (atom {:state ""
+;                          :string ""
+;                          :registers (atom {})
+;                          :hold-register '()
+;                          :level ""
+;                          :pop-config ""
+;                          :push-acts ""
+;                          :sendr-acts (atom {})
+;                          :liftr-acts (atom {} )}))
+
+;Global parse trees
+;(def parse-trees (atom nil))
+(def ^{:dynamic true} *parse-trees* nil)
 
 ;Default starting state in grammar
 (def global-state (atom ""))
+;(def ^{:dynamic true} *global-state* nil)
 
 ;Default trace level
 (def trace-level (atom 0))
+;(def ^{:dynamic true} *trace-level* nil)
 
-(def parse-trees (atom nil))
-
-;Hashtable for the ATN arcs
+;;Hashtable for the ATN arcs
+;;How it should look after initializing
 ;(def atn-arcs-ht (atom {:s/_ '()
 ;                        :s/np '()
 ;                        :s/aux '()
@@ -39,7 +45,8 @@
 ;                        :vp/to '()
 ;                        :comp/_ '()}))
 
-(def atn-arcs-ht (atom nil))
+;(def atn-arcs-ht (atom nil))
+(def ^{:dynamic true} *atn-arcs-ht* nil)
 
 (defrecord config
   [state                                                    ;state of the ATN
@@ -52,10 +59,16 @@
    sendr-acts                                               ;assoc list of (register value) to be set on entry to this config
    liftr-acts])                                             ;assoc list of (register value) to be set on exit from this config
 
+(defn make-configuration
+  [& {:keys [state string registers hold-register level pop-config push-acts sendr-acts liftr-acts]
+      :or {state "", string "", registers (atom {}), hold-register (), level "",
+           pop-config "", push-acts "", sendr-acts (atom {}), liftr-acts (atom {})}}]
+  (config. state string registers hold-register level pop-config push-acts sendr-acts liftr-acts))
+
 (defmacro cat-category [arc]
   `(second (~arc)))
 (defmacro cat-test [arc]
-  `(third (~arc)))
+  `(nth (~arc) 2))
 (defmacro cat-actions [arc]
   `(rest (rest (rest (~arc)))))
 
@@ -136,76 +149,157 @@
   `(rest (rest (rest (rest (~arc))))))
 
 (defmacro set-state
-  ([value] `(swap! global-config assoc :state ~value))
-  ([value config] `(swap! ~config assoc :state ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:state ~configuration) ~value)
+    `(reset! (:state *config*) ~value))) ;; Leave @*config* unevaluated!
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-state
-  ([] `(:state global-config))
-  ([config] `(:state ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:state ~configuration)
+    `@(:state *config*))) ;; Leave *config* unevaluated!
 
 (defmacro set-string
-  ([value] `(swap! global-config assoc :string ~value))
-  ([value config] `(swap! ~config assoc :string ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:string ~configuration) ~value)
+    `(reset! (:string *config*) ~value))) ;; Leave @*config* unevaluated!
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-string
-  ([] `(:string global-config))
-  ([config] `(:string ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:string ~configuration)
+    `@(:string *config*))) ;; Leave *config* unevaluated!
 
 (defmacro set-registers
-  ([value] `(swap! global-config assoc :registers ~value))
-  ([value config] `(swap! ~config assoc :registers ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:registers ~configuration) ~value)
+    `(reset! (:registers *config*) ~value)))
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-registers
-  ([] `(:registers global-config))
-  ([config] `(:registers ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:registers ~configuration)
+    `@(:registers *config*))) ;; Leave *config* unevaluated!
 
 (defmacro set-hold-register
-  ([value] `(swap! global-config assoc :hold-register ~value))
-  ([value config] `(swap! ~config assoc :hold-register ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:hold-register ~configuration) ~value)
+    `(reset! (:hold-register *config*) ~value))) ;; Leave @*config* unevaluated!
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-hold-register
-  ([] `(:hold-register global-config))
-  ([config] `(:hold-register ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:hold-register ~configuration)
+    `@(:hold-register *config*))) ;; Leave *config* unevaluated!
 
 (defmacro set-level
-  ([value] `(swap! global-config assoc :level ~value))
-  ([value config] `(swap! ~config assoc :level ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:level ~configuration) ~value)
+    `(reset! (:level *config*) ~value))) ;; Leave @*config* unevaluated!
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-level
-  ([] `(:level global-config))
-  ([config] `(:level ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:level ~configuration)
+    `@(:level *config*))) ;; Leave *config* unevaluated!
 
 (defmacro set-pop-config
-  ([value] `(swap! global-config assoc :pop-config ~value))
-  ([value config] `(swap! ~config assoc :pop-config ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:pop-config ~configuration) ~value)
+    `(reset! (:pop-config *config*) ~value))) ;; Leave @*config* unevaluated!
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-pop-config
-  ([] `(:pop-config global-config))
-  ([config] `(:pop-config ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:pop-config ~configuration)
+    `@(:pop-config *config*))) ;; Leave *config* unevaluated!
 
 (defmacro set-push-acts
-  ([value] `(swap! global-config assoc :push-acts ~value))
-  ([value config] `(swap! ~config assoc :push-acts ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:push-acts ~configuration) ~value)
+    `(reset! (:push-acts *config*) ~value))) ;; Leave @*config* unevaluated!
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-push-acts
-  ([] `(:push-acts global-config))
-  ([config] `(:pop-config ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:push-acts ~configuration)
+    `@(:push-acts *config*))) ;; Leave *config* unevaluated!
 
 (defmacro set-sendr-acts
-  ([value] `(swap! global-config assoc :sendr-acts ~value))
-  ([value config] `(swap! ~config assoc :sendr-acts ~value)) )
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:sendr-acts ~configuration) ~value)
+    `(reset! (:sendr-acts *config*) ~value))) ;; Leave @*config* unevaluated!
 
+;; Note: Assumes *config* has a non-nil value.
 (defmacro get-sendr-acts
-  ([] `(:sendr-acts global-config))
-  ([config] `(:sendr-acts ~config)))
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:sendr-acts ~configuration)
+    `@(:sendr-acts *config*))) ;; Leave *config* unevaluated!
 
-(defmacro set-liftr-actions
-  ([value] `(swap! global-config assoc :liftr-acts ~value))
-  ([value config] `(swap! ~config assoc :liftr-acts ~value)) )
+(defmacro set-liftr-acts
+  "Set the registers to a given value, either in the provided configuration
+   or, if no configuration was provided, the global configuration."
+  [value & {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `(reset! (:liftr-acts ~configuration) ~value)
+    `(reset! (:liftr-acts *config*) ~value))) ;; Leave @*config* unevaluated!
 
-(defmacro get-liftr-actions
-  ([] `(:liftr-acts global-config))
-  ([config] `(:liftr-acts ~config)))
+;; Note: Assumes *config* has a non-nil value.
+(defmacro get-liftr-acts
+  "Get the registers from a provided configuration, or, if none is provided, then
+   the global configuration."
+  [& {:keys [configuration] :or {configuration nil}}]
+  (if configuration
+    `@(:liftr-acts ~configuration)
+    `@(:liftr-acts *config*))) ;; Leave *config* unevaluated!
 
 (defmacro putarc [state arc]
   `(swap! atn-arcs-ht assoc (keyword ~state) (cons ~arc ((keyword ~state) atn-arcs-ht))))
@@ -213,12 +307,52 @@
 (defmacro getarcs [state]
   `((keyword ~state) atn-arcs-ht))
 
-(defmacro setr [register & forms]
-  `(set-registers
-     ()))
+(defn setup-forms
+  ""
+  [forms]
+  (if (or (symbol? forms) (char? forms) (number? forms) (string? forms)) ;; Equivalent of atoms in Lisp.
+    forms
+    (if (rest forms)
+      (if *parse-trees*
+        forms
+        (flatten forms))
+      (if *parse-trees*
+        (first forms)
+        (flatten (first forms))))))
 
-(defmacro setrq [register & form]
-  )
+(defn internal-getr
+  "Retrieve the specific register from the register map."
+  [register]
+  (get (get-registers) register))
+
+;;Evaluates an ATN form
+(defn evaluate-form
+  "Given an S-expression, evaluates it by:
+    If it's a string or number, just returning those values;
+	If it names a register, returning the register value;
+	If it is *, returning the register value of *;
+	If it is a list, evaluating it as a Clojure form;
+	If it is bound to a variable, returning its value;
+	Otherwise, nil."
+  [s-exp]
+  (cond
+    (number? s-exp) s-exp
+    (string? s-exp) s-exp
+    (internal-getr s-exp) (internal-getr s-exp)
+    (= s-exp '*) (internal-getr '*)
+    (seq? s-exp) (eval s-exp) ;; No need for this peval thing.
+    (resolve s-exp) (resolve s-exp)
+    :default nil))
+
+(defmacro setr [register & forms]
+  `(set-registers (assoc (get-registers)                    ;;map
+                    '~register                              ;;key
+                    (setup-forms (map evaluate-form '~forms))))) ;;val(s)
+
+(defmacro setrq [register & forms]
+  `(set-registers (assoc (get-registers)                    ;;map
+                    '~register                              ;;key
+                    (setup-forms (map '~forms)))))          ;;val(s)
 
 (defmacro addr [register & forms]
   `(let [contents (get-in global-config [:registers (keyword ~register)])
@@ -237,70 +371,71 @@
   )
 
 (defmacro sendr [register & forms]
-  `())
+  `(if ~forms
+     `(set-sendr-acts (assoc (get-sendr-acts)               ;map
+                       '~register                           ;key
+                       (setup-forms (map evaluate-form '~forms)))) ;val(s)
+     `(set-sendr-acts (assoc (get-sendr-acts)               ;map
+                        '~register                          ;key
+                        (internal-getr ~register)))))       ;val(s)
 
 (defmacro sendrq [register & forms]
-  `())
+  `(set-sendr-acts (assoc (get-sendr-acts)                  ;map
+                    '~register                              ;key
+                    (setup-forms (map '~forms)))))          ;val(s)
 
 (defmacro liftr [register & forms]
-  `('(~state)))
+  `(if ~forms
+     `(set-liftr-acts (assoc (get-liftr-acts)               ;map
+                        '~register                          ;key
+                        (setup-forms (map evaluate-form '~forms)))) ;val(s)
+     `(set-liftr-acts (assoc (get-liftr-acts)               ;map
+                        '~register                          ;key
+                        (internal-getr ~register)))))       ;val(s)
 
 (defmacro hold [constit-type form]
-  `(set-hold-register (cons (list ~category
+  `(set-hold-register (cons (list ~constit-type
                                   (list (setup-forms (evaluate-form ~form)))
                                   (get-level))
                             (get-hold-register))))
 
-(defn any-holds? []
-  (binding global-config)
-  (let [holdr (get-hold-register)
-        level (get-level)]
-    (if (nil? holdr)
-      true
-      ((fn [x] (equal (first (rest (rest x))) level)) holdr))))
+;(defn any-holds? []
+;  (binding global-config)
+;  (let [holdr (get-hold-register)
+;        level (get-level)]
+;    (if (nil? holdr)
+;      true
+;      ((fn [x] (equal (first (rest (rest x))) level)) holdr))))
 
-(defmacro to
-  ([state]
-   `())
-  ([state form]
-   `()))
+;(defmacro to
+;  ([state]
+;   `())
+;  ([state form]
+;   `()))
+;
+;(defmacro jump [state]
+;  `())
+;
+;;;(defmacro verify [form] `())
+;
+;(defmacro getr
+;  ([register]
+;   `())
+;  ([register level])
+;  `())
+;
+;(defmacro getf
+;  ([feature]
+;   `())
+;  ([feature word]
+;   `()))
+;
+;(defmacro buildq [exp]
+;  `())
+;
+;(defn bldq [exp]
+;  `())
 
-(defmacro jump [state]
-  `())
-
-;;(defmacro verify [form] `())
-
-(defmacro getr
-  ([register]
-   `())
-  ([register level])
-  `())
-
-(defmacro getf
-  ([feature]
-   `())
-  ([feature word]
-   `()))
-
-(defmacro buildq [exp]
-  `())
-
-(defn bldq [exp]
-  `())
-
-(defn setup-forms [forms]
-  (if (atom forms)
-    forms
-    (if (rest forms)
-      (if parse-trees
-        forms (flatten forms))
-      (if parse-trees
-        (first forms)
-        (flatten (first forms))))))
-
-;;Evaluates an ATN form
-(defn evaluate-forms [s-exp]
-  )
 
 ;;Clojure has a function that yields the unevaluated form (quote(____))
 ;;(defmacro quote [value])
@@ -309,8 +444,8 @@
 (defmacro nullr [register]
   `(= (get-in global-config [:registers (keyword ~register)]) nil))
 
-(defmacro end-of-sentence []
-  (binding [global-config]))
+;(defmacro end-of-sentence []
+;  (binding [global-config]))
 
 ;;DO LATER/WHEN IT COMES UP
 ;;(defmacro checkf [feature value])
